cbce5613a6e290f58490df05f21d35b43deec122
Andrew Fink <andrewfink@Andrews-MacBook-Pro.local>
2022-07-05 15:40:48 -0500

    refactoring maps to a plugin

--- .nuxt/index.js
+++ .nuxt/index.js
@@ -13,6 +13,7 @@ import { setContext, getLocation, getRouteData, normalizeError } from './utils'
 /* Plugins */
 
 import nuxt_plugin_plugin_763b26cc from 'nuxt_plugin_plugin_763b26cc' // Source: ./components/plugin.js (mode: 'all')
+import nuxt_plugin_maps_6ed989cc from 'nuxt_plugin_maps_6ed989cc' // Source: ../plugins/maps.client (mode: 'client')
 
 // Component: <ClientOnly>
 Vue.component(ClientOnly.name, ClientOnly)
@@ -178,6 +179,10 @@ async function createApp(ssrContext, config = {}) {
 
   if (typeof nuxt_plugin_plugin_763b26cc === 'function') {
     await nuxt_plugin_plugin_763b26cc(app.context, inject)
+  }
+
+  if (process.client && typeof nuxt_plugin_maps_6ed989cc === 'function') {
+    await nuxt_plugin_maps_6ed989cc(app.context, inject)
   }
 
   // Lock enablePreview in context
--- pages/Home/_id.vue
+++ pages/Home/_id.vue
@@ -19,15 +19,6 @@ export default {
     head(){
         return {
             title: this.home.title,
-            script: [{
-                src:"https://maps.googleapis.com/maps/api/js?key=AIzaSyD6z516Q9eCQNmenDh5p_jcc26892hOlfw&Libraries=places&callback=initMap",
-                hid: "map",
-                defer: true,
-                skip: process.client && window.mapLoaded
-            }, {
-                innerHTML: "window.initMap = function(){window.mapLoaded = true}",
-                hid: "map-init",
-            }],
         }
     },
     data(){
@@ -36,16 +27,7 @@ export default {
         }
     },
     mounted(){
-        const mapOptions = {
-            zoom: 18,
-            center: new window.google.maps.LatLng(this.home._geoloc.lat, this.home._geoloc.lng),
-            disableDefaultUI: true,
-            zoomControl: true
-        }
-        const map = new window.google.maps.Map(this.$refs.map, mapOptions)
-        const position = new window.google.maps.LatLng(this.home._geoloc.lat, this.home._geoloc.lng)
-        const marker = new window.google.maps.Marker({position})
-        marker.setMap(map)
+        this.$maps.showMap(this.$refs.map, this.home._geoloc.lat, this.home._geoloc.lng)
     },
     created(){
         const home = homes.find((home) => home.objectID == this.$route.params.id)
